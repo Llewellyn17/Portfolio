f7be981946247c1934ef379d268f7fbdeac85378
llewellyn17 <llewellyn.isaac@yahoo.com>
2019-03-18 13:34:35 -0500

    push it

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const port = '3001';
 
 app.get('/', function (req, res) {
-    res.sendFile(dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static(path.join(dirname + '/')));
+app.use(express.static(path.join(__dirname + '/')));
 app.listen(process.env.PORT || 5000, function () {
     console.log('Server listening on port ' + port);
 });
